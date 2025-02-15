f3670f6e2c74567d7381054653e33ce7b93a790d
Christian R <cool9373guy@gmail.com>
2025-02-14 18:28:16 -0800

    ROUTING
    YESSSSS

---
+++ client/src/CatsNDogs.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+
+function CatsNDogs() {
+
+//consts for saving values go here
+
+const [bingus, SetBingus] = useState<number>();
+
+
+
+  return (
+    <>
+   <p>No cats, no dogs</p>
+  </>
+  
+);
+}
+
+export default CatsNDogs
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,45 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import CatsNDogs from './CatsNDogs.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import RootMenu from './RootMenu.tsx'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Root from './Root.tsx'
 
+
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: <Root />,
+      children: [
+          {
+              index: true,
+              element: <RootMenu />,
+          },
+          {
+              path: 'admin',
+              element: <App />,
+          },
+          {
+              path: 'Gatos',
+              element: <CatsNDogs />,
+          },
+         
+      ],
+  },
+]);
+
+
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
